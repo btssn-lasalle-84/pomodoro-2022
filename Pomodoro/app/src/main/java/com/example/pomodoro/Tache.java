7b744e726ccdf9b1c90d5d69f7acb5e22ee7bc27
“testablet” <teddyestablet84@gmail.com>
2022-04-07 15:15:49 +0200

    Modification IHM & Ajout de la classe Tache.java

---
+++ Pomodoro/app/src/main/java/com/example/pomodoro/Tache.java
@@ -0,0 +1,23 @@
+package com.example.pomodoro;
+
+public class Tache
+{
+    private String _idTache = "#Tache1";
+    private String _nom = "Tache 1";
+    private String couleurTache = "Rouge"; //couleur disponible : Bleue, Noire, Rouge, Vert
+    private int _tempsTache = 25; //valeur exprimée en minutes | ne peux pas être plus grand que 50
+    private int _tempsPauseCourte = 5; //valeur exprimée en minutes | ne peux pas être plus grand que 20
+    private int _tempsPauseLongue = 10; //valeur exprimée en minutes | ne peux pas être plus grand que 30
+    private int _nombreDeCycles = 4;
+    private int _nombreDeSessions = 4;
+
+    
+
+    public void editerTaches()
+    {
+    }
+
+    public void supprimerTache()
+    {
+    }
+}
