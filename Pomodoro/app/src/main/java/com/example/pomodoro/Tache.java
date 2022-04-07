dd879055c3f2365471b5c44995356c191870ddbc
tvaira <tvaira@free.fr>
2022-04-07 22:05:18 +0200

    POMO22-12 Améliore l'IHM

--- Pomodoro/app/src/main/java/com/example/pomodoro/Tache.java
+++ Pomodoro/app/src/main/java/com/example/pomodoro/Tache.java
@@ -1,23 +1,64 @@
 package com.example.pomodoro;
 
+/**
+ * @file PomodoroActivity.java
+ * @brief Déclaration de la classe PomodoroActivity
+ * @author Teddy ESTABLET
+ */
+
+import android.util.Log;
+
+/**
+ * @class Tache
+ * @brief Définit le concept de tâche
+ */
 public class Tache
 {
-    private String _idTache = "#Tache1";
-    private String _nom = "Tache 1";
-    private String couleurTache = "Rouge"; //Couleur disponible : Bleue, Noire, Rouge, Vert
-    private int _tempsTache = 25; //Valeur exprimée en minutes | ne peux pas être plus grand que 50
-    private int _tempsPauseCourte = 5; //Valeur exprimée en minutes | ne peux pas être plus grand que 20
-    private int _tempsPauseLongue = 10; //Valeur exprimée en minutes | ne peux pas être plus grand que 30
-    private int _nombreDeCycles = 4;
-    private int _nombreDeSessions = 4;
+    /**
+     * Constantes
+     */
+    private static final String TAG = "_Tache";  //!< TAG pour les logs
+    public enum CouleurTache {
+        Bleue, Noire, Rouge, Verte;
+    }
 
+    /**
+     * Attributs
+     */
+    //private String _idTache = "#Tache1"; //!< ???
+    private String nom = "Tache"; //!< Le nom de la tâche
+    private CouleurTache couleurTache = CouleurTache.Rouge; //!< La couleur par défaut
+    private int tempsTache = 25; //!< Valeur exprimée en minutes | ne peux pas être plus grand que 50
+    private int tempsPauseCourte = 5; //!< Valeur exprimée en minutes | ne peux pas être plus grand que 20
+    private int tempsPauseLongue = 10; //!< Valeur exprimée en minutes | ne peux pas être plus grand que 30
+    private int nombreDeCycles = 4; //!<
+    private int nombreDeSessions = 4; //!<
 
+    /**
+     * @brief Constructeur
+     */
+    public void Tache()
+    {
+    }
+
+    public String getNom()
+    {
+        return this.nom;
+    }
+
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
 
     public void editerTaches()
     {
+        Log.d(TAG, "editerTaches()");
     }
 
     public void supprimerTache()
     {
+        Log.d(TAG, "supprimerTache()");
+        this.nom = "";
     }
 }
