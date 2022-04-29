3026dd07d9c5259eb4b6a48ead6ad4e92aaddab8
“testablet” <teddyestablet84@gmail.com>
2022-04-29 16:16:42 +0200

    Modification des fichiers Peripherique.java et TReception.java / Ajout des méthodes connecter(), deconnecter(), envoyer() et onClick()| Travail sur le bluetooth

--- Pomodoro/app/src/main/java/com/example/pomodoro/TReception.java
+++ Pomodoro/app/src/main/java/com/example/pomodoro/TReception.java
@@ -9,12 +9,16 @@ package com.example.pomodoro;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @class TReception
@@ -25,10 +29,14 @@ public class TReception extends Thread
     private static final String TAG = "_TReception";  //!< TAG pour les logs
 
     Handler handlerUI;
+    Handler handler;
+    Socket socket;
     InputStream receiveStream;
+    OutputStream sendStream;
+    Thread tReception;
     private boolean fini;
 
-    TReception(Handler h, InputStream receiveStream)
+    TReception(Handler h, InputStream receiveStream, OutputStream sendStreal)
     {
         this.handlerUI = h;
         this.receiveStream = receiveStream;
@@ -89,7 +97,7 @@ public class TReception extends Thread
         }
     }
 
-    /**public void connecter()
+    public void connecter()
     {
         new Thread()
         {
@@ -102,7 +110,7 @@ public class TReception extends Thread
                     socket.connect();
 
                     Message msg = Message.obtain();
-                    msg.arg1 = CODE_CONNEXION;
+                    msg.arg1 = Peripherique.CODE_CONNEXION;
                     handler.sendMessage(msg);
 
                     tReception.start();
@@ -110,10 +118,70 @@ public class TReception extends Thread
                 }
                 catch(IOException e)
                 {
-                    System.out.println("<Socket> error connect");
+                    System.out.println("<Socket> erreur de connection");
                     e.printStackTrace();
                 }
             }
         }.start();
-    }**/
+    }
+    public boolean deconnecter()
+    {
+        try
+        {
+            // @todo à modifier
+            tReception.arreter();
+            socket.close();
+            return true;
+        }
+        catch(IOException e)
+        {
+            System.out.println("<Socket> erreur de fermeture");
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public void envoyer(String data)
+    {
+        if(socket == null)
+            return;
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    if(socket.isConnected())
+                    {
+                        sendStream.write(data.getBytes());
+                        sendStream.flush();
+                    }
+                }
+                catch(IOException e)
+                {
+                    System.out.println("<Socket> erreur d'envoi");
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+    // @todo à retravailler
+    public void onClick(View v)
+    {
+        if(v.getId() == R.id.buttonConnecter)
+        {
+            peripherique.connecter();
+        }
+
+        if(v.getId() == R.id.buttonDeconnecter)
+        {
+            if(peripherique.deconnecter())
+            {
+                btnConnecter.setEnabled(true);
+                btnDeconnecter.setEnabled(false);
+            }
+        }
+
+        gererBoutons(v);
+    }
 }
