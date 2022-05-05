edec33b1f5cb9d76ea34442170a465545212ab03
“testablet” <teddyestablet84@gmail.com>
2022-05-05 16:04:38 +0200

    Ajout de la classe Protocole.java

---
+++ Pomodoro/app/src/main/java/com/example/pomodoro/Protocole.java
@@ -0,0 +1,23 @@
+package com.example.pomodoro;
+
+/**
+ * @file Protocole.java
+ * @brief Déclaration de la classe Protocole
+ * @author Teddy ESTABLET
+ */
+
+
+/**
+ * @class Protocole
+ * @brief Défini le concept de protocole
+ */
+
+public class Protocole
+{
+    /**
+     * @brief Constantes
+     */
+    private static String debutTrame = "#";
+    private static String finTrame = "\r\n";
+    private static String delimiteurTrame = "&";
+}
